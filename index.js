db443fc76699b58b61574b7a0aa291310f6e2fcd
wael kamira <ramondm4@gmail.com>
2023-04-16 19:23:03 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    return win ? win.year : undefined;
+  }
+
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
